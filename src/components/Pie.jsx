34afe9a7464afbbfa621d5cab939a71e7ada43d0
whejy <john.wheldale@outlook.com>
2023-02-27 15:26:06 +1030

    add fixed colours to pie slices

--- src/components/Pie.jsx
+++ src/components/Pie.jsx
@@ -1,4 +1,4 @@
-import { VictoryPie, VictoryTheme, VictoryLabel } from 'victory-native';
+import { VictoryPie, VictoryLabel } from 'victory-native';
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import CategoryDetails from './CategoryDetails';
@@ -6,6 +6,13 @@ import FormToggle from './FormToggle';
 import AddExpense from './AddExpense';
 import Dates from './Dates';
 import AlterPieList from './AlterPieList';
+import theme from '../../theme';
+
+// const styles = StyleSheet.create({
+//   separator: {
+//     height: 10,
+//   },
+// });
 
 const Pie = ({ data, updatePie, removePie }) => {
   const [category, setCategory] = useState('');
@@ -22,21 +29,27 @@ const Pie = ({ data, updatePie, removePie }) => {
 
   const formatPieData = () => {
     let pieData = [];
-    const remainingIncome = pieData.reduce((acc, curr) => acc - curr.y, income);
 
     for (const category in expenses) {
       expenses[category].total = expenses[category].reduce(
         (acc, curr) => acc + curr.cost,
         0
       );
-      pieData.push({ x: category, y: expenses[category].total });
+      pieData.push({
+        x: category,
+        y: expenses[category].total,
+        fill: theme.colors.pieData[category],
+      });
     }
+
+    const remainingIncome = pieData.reduce((acc, curr) => acc - curr.y, income);
 
     pieData
       .sort((a, b) => b.y - a.y)
       .push({
         x: 'Income',
         y: remainingIncome,
+        fill: theme.colors.pieData.Income,
       });
 
     return { pieData, remainingIncome };
@@ -106,7 +119,7 @@ const Pie = ({ data, updatePie, removePie }) => {
         <Dates start={data.dates.startDate} end={data.dates.endDate} />
       </Text>
       <VictoryPie
-        theme={VictoryTheme.material}
+        // theme={VictoryTheme.material}
         // colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
         data={pieData}
         events={events}
@@ -114,6 +127,9 @@ const Pie = ({ data, updatePie, removePie }) => {
           `${datum.x} \n$${datum.y.toLocaleString('en-US')}`
         }
         style={{
+          data: {
+            fill: ({ datum }) => datum.fill,
+          },
           labels: { fontSize: 16 },
           parent: { border: '1px solid #ccc', paddingBottom: 0 },
         }}
--- theme.js
+++ theme.js
@@ -1,5 +1,19 @@
 import { Platform } from 'react-native';
 
+const sliceColors = {
+  Bills: '#ffcbcb',
+  Entertainment: '#e6b2c6',
+  Food: '#ffb5b5',
+  Gifts: '#f2f2f2',
+  Income: '#79c2d0',
+  Medical: '#ffe9e3',
+  Other: '#e3f6f5',
+  Personal: '#d4ebd0',
+  Savings: '#bbe4e9',
+  Shopping: '#cadefc',
+  Transport: '#c3bef0',
+};
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -7,6 +21,7 @@ const theme = {
     error: '#d73a4a',
     primary: '#0366d6',
     appBar: '#24292e',
+    pieData: sliceColors,
   },
   fontSizes: {
     body: 14,
